3f9a4d59a8aab1fd83f4755e5ec028d1fc17fc08
admin <1596782257@qq.com>
2021-10-07 12:37:29 +0800

    💄 Site > Fix typo

--- src/script.js
+++ src/script.js
@@ -5,8 +5,9 @@ import { GUI } from "three/examples/jsm/libs/dat.gui.module.js";
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
-import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js';
+import { TWEEN } from "three/examples/jsm/libs/tween.module.min.js";
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader.js";
+import { MapControls } from "./MapControls"; //拖动camera 控制
 
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
@@ -83,19 +84,23 @@ function addArea(x, z, width, length, scene, name, textColor, font_size) {
   });
 }
 
-function skybox(){
+function skybox() {
   // skybox
 
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-cubeTextureLoader.setPath( 'images/' );
+  const cubeTextureLoader = new THREE.CubeTextureLoader();
+  cubeTextureLoader.setPath("images/");
 
-const cubeTexture = cubeTextureLoader.load( [
-  "px.jpg", "nx.jpg",
-  "py.jpg", "ny.jpg",
-  "pz.jpg", "nz.jpg"
-] );
-scene.background = cubeTexture;
+  const cubeTexture = cubeTextureLoader.load([
+    "px.jpg",
+    "nx.jpg",
+    "py.jpg",
+    "ny.jpg",
+    "pz.jpg",
+    "nz.jpg",
+  ]);
+  scene.background = cubeTexture;
 }
+
 
 function init() {
   container = document.createElement("div");
@@ -113,15 +118,13 @@ function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xcce0ff);
   scene.fog = new THREE.Fog(scene.background, 1, 5000);
-  
-  skybox();//天空盒
+
+  skybox(); //天空盒
 
   camera = new THREE.PerspectiveCamera(75, width / height, 1, 100);
   camera.position.set(4, 4, 5);
 
-
-  
-
+  //controls 
   controls = new OrbitControls(camera, renderer.domElement);
   controls.minDistance = 5;
   controls.maxDistance = 20;
@@ -129,6 +132,8 @@ function init() {
   controls.enablePan = false;
   controls.enableDamping = true;
   controls.dampingFactor = 0.05;
+
+
 
   // LIGHTS HemisphereLight
 
@@ -417,3 +422,111 @@ function animate() {
 
   stats.end();
 }
+
+
+
+
+
+// let camera, controls, scene, renderer;
+
+// 			init();
+// 			//render(); // remove when using next line for animation loop (requestAnimationFrame)
+// 			animate();
+
+// 			function init() {
+
+// 				scene = new THREE.Scene();
+// 				scene.background = new THREE.Color( 0xcccccc );
+// 			//	scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+
+// 				renderer = new THREE.WebGLRenderer( { antialias: true } );
+// 				renderer.setPixelRatio( window.devicePixelRatio );
+// 				renderer.setSize( window.innerWidth, window.innerHeight );
+// 				document.body.appendChild( renderer.domElement );
+
+// 				camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 5000 );
+// 				camera.position.set( 400, 200, 0 );
+
+// 				// controls
+
+// 				controls = new MapControls( camera, renderer.domElement );
+
+// 				//controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+// 				controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+// 				controls.dampingFactor = 0.5;
+
+// 				controls.screenSpacePanning = false;
+
+// 				controls.minDistance = 100;
+// 				controls.maxDistance = 500;
+
+// 				controls.maxPolarAngle = Math.PI / 2;
+
+// 				// world
+
+// 				const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// 				geometry.translate( 0, 0.5, 0 );
+// 				const material = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } );
+
+// 				for ( let i = 0; i < 500; i ++ ) {
+
+// 					const mesh = new THREE.Mesh( geometry, material );
+// 					mesh.position.x = Math.random() * 1600 - 800;
+// 					mesh.position.y = 0;
+// 					mesh.position.z = Math.random() * 1600 - 800;
+// 					mesh.scale.x = 20;
+// 					mesh.scale.y = Math.random() * 80 + 10;
+// 					mesh.scale.z = 20;
+// 					mesh.updateMatrix();
+// 					mesh.matrixAutoUpdate = false;
+// 					scene.add( mesh );
+
+// 				}
+
+// 				// lights
+
+// 				const dirLight1 = new THREE.DirectionalLight( 0xffffff );
+// 				dirLight1.position.set( 1, 1, 1 );
+// 				scene.add( dirLight1 );
+
+// 				const dirLight2 = new THREE.DirectionalLight( 0x002288 );
+// 				dirLight2.position.set( - 1, - 1, - 1 );
+// 				scene.add( dirLight2 );
+
+// 				const ambientLight = new THREE.AmbientLight( 0x222222 );
+// 				scene.add( ambientLight );
+
+// 				//
+
+// 				window.addEventListener( 'resize', onWindowResize );
+
+// 				const gui = new GUI();
+// 				gui.add( controls, 'screenSpacePanning' );
+
+// 			}
+
+// 			function onWindowResize() {
+
+// 				camera.aspect = window.innerWidth / window.innerHeight;
+// 				camera.updateProjectionMatrix();
+
+// 				renderer.setSize( window.innerWidth, window.innerHeight );
+
+// 			}
+
+// 			function animate() {
+
+// 				requestAnimationFrame( animate );
+
+// 				controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+
+// 				render();
+
+// 			}
+
+// 			function render() {
+
+// 				renderer.render( scene, camera );
+
+// 			}
